010f7fa40e4eea9e97a41e2d6731489a79f11340
zaleskiy88 <zaleskiy88@gmail.com>
2023-03-19 20:05:08 +0000

    moved secrets to .env file

--- helpers/sendEmail.js
+++ helpers/sendEmail.js
@@ -2,22 +2,22 @@ const nodemailer = require("nodemailer");
 require("dotenv").config("");
 // ====================Email sender========================================= //
 
-// const { SAPO_EMAIL_ADRESS, SAPO_EMAIL_PASS, SAPO_SMTP_SERVER } = process.env;
+const { SAPO_EMAIL_ADRESS, SAPO_EMAIL_PASS, SAPO_SMTP_SERVER } = process.env;
 
 const nodemailerConfig = {
-  host: "smtp.sapo.pt", // SAPO_SMTP_SERVER
+  host: SAPO_SMTP_SERVER,
   port: 465,
   secure: true,
   auth: {
-    user: "zaleskiy88@sapo.pt", // SAPO_EMAIL_ADRESS
-    pass: "Sl4vaUkraini", // SAPO_EMAIL_PASS
+    user: SAPO_EMAIL_ADRESS,
+    pass: SAPO_EMAIL_PASS,
   },
 };
 
 const transporter = nodemailer.createTransport(nodemailerConfig);
 
 const sendEmail = async (data) => {
-  const email = { ...data, from: "zaleskiy88@sapo.pt" }; // SAPO_EMAIL_ADRESS
+  const email = { ...data, from: SAPO_EMAIL_ADRESS };
 
   await transporter.sendMail(email);
 
